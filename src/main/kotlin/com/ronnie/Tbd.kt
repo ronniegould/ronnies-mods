7709b24fa463b3cb2f0576e9e29898632a34cbe5
MatthewGould123 <matt@saowapan.com>
2024-03-01 16:54:04 +0700

    add a custom item to test on ronnies laptop

--- src/main/kotlin/com/ronnie/Tbd.kt
+++ src/main/kotlin/com/ronnie/Tbd.kt
@@ -2,9 +2,16 @@ package com.ronnie
 
 import com.ronnie.potions.TbdPotions
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.item.Item
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
 
 object Tbd : ModInitializer {
 	const val MOD_ID = "tbd"
+
+	private val CUSTOM_ITEM = Registry.register(Registries.ITEM, Identifier(MOD_ID, "custom_item"), Item(FabricItemSettings()));
 
 	override fun onInitialize() {
 		TbdPotions.registerPotions()
