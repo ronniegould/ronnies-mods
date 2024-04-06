4cafb9762ff67cc25b1712bc4ed1055cd027c3f7
MatthewGould123 <matt@saowapan.com>
2024-04-06 12:32:59 +0700

    Test adding enchantments to armour

--- src/main/kotlin/com/ronnie/Tbd.kt
+++ src/main/kotlin/com/ronnie/Tbd.kt
@@ -3,16 +3,40 @@ package com.ronnie
 import com.ronnie.items.TbdRegisterItems
 import com.ronnie.potions.TbdPotions
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents.Modify
+import net.fabricmc.fabric.api.loot.v2.LootTableSource
+import net.minecraft.loot.LootManager
+import net.minecraft.loot.LootPool
+import net.minecraft.loot.LootTable
+import net.minecraft.loot.entry.ItemEntry
+import net.minecraft.resource.ResourceManager
+import net.minecraft.util.Identifier
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+
 
 object Tbd : ModInitializer {
 	const val MOD_ID = "tbd"
 	val LOGGER: Logger = LogManager.getLogger(MOD_ID)
 
+	private val STRONGHOLD_LIBRARY_CHEST_LOOT_TABLE_ID = Identifier("minecraft", "chests/stronghold_library")
+
 	override fun onInitialize() {
 		TbdPotions.registerPotions()
 		TbdPotions.registerPotionsRecipes()
 		TbdRegisterItems.registerItems()
+
+
+		LootTableEvents.MODIFY.register(Modify { resourceManager: ResourceManager?, lootManager: LootManager?, id: Identifier, tableBuilder: LootTable.Builder, source: LootTableSource ->
+			if (STRONGHOLD_LIBRARY_CHEST_LOOT_TABLE_ID == id) {
+				val pool: LootPool.Builder = LootPool.builder()
+					.with(ItemEntry.builder(TbdRegisterItems.helmet))
+
+				// Add the loot pool to the loot table
+				tableBuilder.pool(pool)
+			}
+		})
+
 	}
 }
--- src/main/kotlin/com/ronnie/items/TbdRegisterItems.kt
+++ src/main/kotlin/com/ronnie/items/TbdRegisterItems.kt
@@ -8,6 +8,8 @@ import net.minecraft.util.Identifier
 
 
 object  TbdRegisterItems {
+    lateinit var helmet: Item
+
     private fun registerItem(name: String, item: Item): Item {
         return Registry.register(Registries.ITEM, Identifier(Tbd.MOD_ID, name), item)
     }
@@ -17,6 +19,6 @@ object  TbdRegisterItems {
         registerItem(SpeedArmour.name, SpeedArmour.item)
         registerItem(SpeedArmour.legsName, SpeedArmour.legsItem)
         registerItem(SpeedArmour.chestName, SpeedArmour.chestItem)
-        registerItem(SpeedArmour.helmetName, SpeedArmour.helmetItem)
+        helmet = registerItem(SpeedArmour.helmetName, SpeedArmour.helmetItem)
     }
 }
